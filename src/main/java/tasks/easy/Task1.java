aabb07cbe7a6320acb1d32d3a76168f9640fb4e0
karin <karinka.kostraba@gmail.com>
2019-07-25 22:16:14 +0300

    two sum

---
+++ src/main/java/tasks/easy/Task1.java
@@ -0,0 +1,22 @@
+package tasks.easy;
+
+public class Task1 {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = 0; j < nums.length; j++) {
+
+                if (i != j) {
+
+                    if ((nums[i] + nums[j]) == target) {
+                        return new int[] {i, j};
+                    }
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+}
