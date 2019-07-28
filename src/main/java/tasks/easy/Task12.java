f2ddbb6979f9ba619e52f04720668db55fee99dc
karinka <karinka.kostraba@gmail.com>
2019-07-28 19:25:17 +0300

    Integer to Roman

---
+++ src/main/java/tasks/easy/Task12.java
@@ -0,0 +1,28 @@
+package tasks.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task12 {
+
+    public static void main(String[] args) {
+
+        System.out.println(intToRoman(128));
+    }
+
+
+    public static String intToRoman(int num) {
+
+        Map<Integer, String> map = new HashMap<>(7);
+        map.put(1, "I");
+        map.put(5, "V");
+        map.put(10, "X");
+        map.put(50, "L");
+        map.put(100, "C");
+        map.put(500, "D");
+        map.put(1000, "M");
+
+
+        return "";
+    }
+}
---
+++ src/test/java/tasks/easy/Task12Test.java
@@ -0,0 +1,21 @@
+package tasks.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static tasks.easy.Task12.intToRoman;
+
+public class Task12Test {
+
+    @Test
+    public void intToRomanTest() {
+
+        Assert.assertEquals("I", intToRoman(1));
+        Assert.assertEquals("V", intToRoman(5));
+        Assert.assertEquals("X", intToRoman(10));
+        Assert.assertEquals("L", intToRoman(50));
+        Assert.assertEquals("C", intToRoman(100));
+        Assert.assertEquals("D", intToRoman(500));
+        Assert.assertEquals("M", intToRoman(1000));
+    }
+}
