4c03c36c46f20d9a3ffb214c8bc3c94bbf66a266
karinka <karinka.kostraba@gmail.com>
2019-07-28 19:05:44 +0300

    Two Sum

--- src/test/java/tasks/easy/Task1Test.java
+++ src/test/java/tasks/easy/Task1Test.java
@@ -1,4 +1,4 @@
-package tasks;
+package tasks.easy;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,11 +11,11 @@ public class Task1Test {
     public void twoSumTest() {
 
         Assert.assertArrayEquals(new int[]{}, new int[]{});
-        Assert.assertArrayEquals(new int[] {0, 1}, twoSum(new int[] {2, 7, 11, 15}, 9));
-        Assert.assertArrayEquals(new int[] {1, 3}, twoSum(new int[] {2, 7, 11, 15}, 22));
+        Assert.assertArrayEquals(new int[]{0, 1}, twoSum(new int[]{2, 7, 11, 15}, 9));
+        Assert.assertArrayEquals(new int[]{1, 3}, twoSum(new int[]{2, 7, 11, 15}, 22));
 
         int[] array = new int[]{2, 3, 4, 11, 15};
         int target = 9;
-        Assert.assertArrayEquals(new int[] {}, twoSum(array, target));
+        Assert.assertArrayEquals(new int[]{}, twoSum(array, target));
     }
 }
